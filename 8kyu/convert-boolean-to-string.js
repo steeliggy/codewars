343230036926dbbe82d866142daf91a7aa80554e
iggy <steeliggyinc@gmail.com>
2024-01-05 23:34:45 +0300

    convert-boolean-to-string

---
+++ 8kyu/convert-boolean-to-string.js
@@ -0,0 +1,8 @@
+//DESCRIPTION
+// Implement a function which convert the given boolean value into its string representation.
+
+
+//SoLUTION
+function booleanToString(b){
+    return b.toString()
+  }
