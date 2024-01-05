53779e6f45885231e1b7e624b35166da395e15fd
iggy <steeliggyinc@gmail.com>
2024-01-06 00:15:33 +0300

    remove-exclamation-marks

---
+++ 8kyu/remove-exclamation-marks.js
@@ -0,0 +1,9 @@
+//DESCRIPTION
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+
+//SOLUTION
+function removeExclamationMarks(s) {
+    let newStr = s.replace("!", "");
+    return newStr;
+}
