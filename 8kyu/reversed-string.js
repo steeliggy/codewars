12a06a57f77baa8d3f017ebca156102a0310f5cd
iggy <steeliggyinc@gmail.com>
2024-01-05 23:19:25 +0300

    reversed-string

---
+++ 8kyu/reversed-string.js
@@ -0,0 +1,13 @@
+//DESCRIPTION
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+function solution(str) {
+    let newStr = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        newStr += str[i];
+    }
+    return newStr;
+}
